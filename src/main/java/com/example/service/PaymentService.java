e30508bd003dcbe1462d3e93ac06531c0b78d842
Иванов Глеб <gleb4832@mail.ru>
2025-10-03 11:10:59 +0400

    NewInit

--- src/main/java/com/example/service/PaymentService.java
+++ src/main/java/com/example/service/PaymentService.java
@@ -3,16 +3,20 @@ package com.example.service;
 import com.example.aspect.LogLevel;
 import com.example.aspect.ToLog;
 import com.example.model.Payment;
+import com.example.model.Resident;
 import com.example.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.DoubleAccumulator;
 
 @Service
 public class PaymentService {
@@ -30,6 +34,16 @@ public class PaymentService {
 
     public Optional toId(Long id) {
         return paymentRepository.findById(id);
+    }
+
+    @ToLog(value = "Обновление информации о платеже", level = LogLevel.INFO)
+    public Payment setInfo(Long id, Resident resident, LocalDate data, Double amount, Payment nwPayment) {
+        nwPayment.setPaymentDate(data);
+        nwPayment.setId(id);
+        nwPayment.setAmount(amount);
+        nwPayment.setResident(resident);
+
+        return paymentRepository.save(nwPayment);
     }
 
     @ToLog(value = "Добавление нового платежа", level = LogLevel.INFO)
